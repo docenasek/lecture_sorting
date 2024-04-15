5048e351cd2416b1d13e5a289b5aa2ee1a9546fe
Dalibor Ocenasek <257032@vutbr.cz>
2024-04-15 12:40:59 +0200

    Added bubble sort

--- sorting.py
+++ sorting.py
@@ -11,6 +11,40 @@ def read_data(file_name):
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
 
+    with open (file_path, "r") as file:
+
+
+
+def selection_sort(list, direction="up"):
+    """
+    :param list: list of numbers
+    :return: list of numbers in an ascending order
+    """
+    n = len(list)
+    for i in range(n):
+        min_idx = 1
+        for num_idx in range(i + 1, n):
+            if direction = "up":
+                if list[num_idx]<list[min_idx]:
+                    min_idx = num_idx
+            if direction = "down":
+                if list[num_idx]>list[min_idx]:
+                    min_idx = num_idx
+            else:
+                return None
+        list[i], list[min_idx] = list[min_idx], list[i]
+
+    return list
+
+
+def bubble_sort(list):
+    n = len(list)
+    for num in range(n - 1):
+        if list[num - 1] < list[num]:
+            list[num - 1], list[num] = list[num], list[num - 1]
+        else:
+            continue
+    return list
 
 def main():
     pass
